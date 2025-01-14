cbc551b572b7253df237d5b307bc3e96db91ba91
maht <Matinkianigame@gmail.com>
2025-01-14 20:09:23 +0330

    Add new tests for dynamic parameter

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -12,11 +12,21 @@ class RouterTest extends TestCase
     public function test_add_and_dispatch_route(): void
     {
         $router = new Router;
-        $router->add('GET', '/test', fn (): string => 'Test Route');
+        $router->add('GET', '/test', fn(): string => 'Test Route');
 
         $response = $router->dispatch('GET', '/test');
 
         $this->assertEquals('Test Route', $response);
+    }
+
+    public function test_add_and_dispatch_route_with_dynamic_parameter(): void
+    {
+        $router = new Router;
+        $router->add('GET', '/test/{id}', fn($id): string => 'Test Route ' . $id);
+
+        $response = $router->dispatch('GET', '/test/123');
+
+        $this->assertEquals('Test Route 123', $response);
     }
 
     public function test_404_for_unmatched_route(): void
@@ -26,12 +36,38 @@ class RouterTest extends TestCase
         $this->assertEquals('404 Not Found', $response);
     }
 
+    public function test_404_for_unmatched_route_with_dynamic_parameter(): void
+    {
+        $router = new Router;
+        $response = $router->dispatch('GET', '/non-existent/123');
+        $this->assertEquals('404 Not Found', $response);
+    }
+
     public function test_404_for_unmatched_method(): void
     {
         $router = new Router;
-        $router->add('GET', '/test', fn (): string => 'Test Route');
+        $router->add('GET', '/test', fn(): string => 'Test Route');
         $response = $router->dispatch('POST', '/test');
-        $this->assertStringNotContainsString('Test Route', $response);
         $this->assertEquals('404 Not Found', $response);
     }
+
+    public function test_404_for_unmatched_method_with_dynamic_parameter(): void
+    {
+        $router = new Router;
+        $router->add('GET', '/test/{id}', fn($id): string => 'Test Route ' . $id);
+        $response = $router->dispatch('POST', '/test/123');
+        $this->assertEquals('404 Not Found', $response);
+    }
+
+    public function test_404_for_unset_dynamic_parameter(): void
+    {
+        $router = new Router;
+        $router->add('GET', '/test/{id}', fn($id): string => 'Test Route ' . $id);
+        $response = $router->dispatch('GET', '/test');
+        $this->assertEquals('404 Not Found', $response);
+    }
+
+
+
+
 }
